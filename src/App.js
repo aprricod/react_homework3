6e119cd3fe2e9cff378b350aeb9392c7bd31b816
Artem <aprricod@gmail.com>
2021-05-19 19:55:19 +0300

    добавлены карточки новостей

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import React from "react";
 import "./App.css";
-import logo from "./logo.svg";
 
-function App() {
+export default function App() {
+  const data = [
+    {
+      title: "Италия",
+      desc: "описание страны",
+    },
+    {
+      title: "Франция",
+      desc: "описание страны",
+    },
+    {
+      title: "Румыния",
+      desc: "описание страны",
+    },
+  ];
+  const newCard = ({ title, desc }) => {
+    return (
+      <div
+        style={{
+          boxSizing: "border-box",
+          padding: "10px",
+          border: "1px solid grey",
+          width: "30%",
+          borderRadius: "10px",
+        }}
+      >
+        <h2>{title}</h2>
+        <p>{desc}</p>
+      </div>
+    );
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Блог о путешествиях</h1>
       </header>
+      <img
+        style={{
+          maxWidth: "100%",
+        }}
+        src="https://prod-virtuoso.dotcmscloud.com/dA/188da7ea-f44f-4b9c-92f9-6a65064021c1/heroImage1/PowerfulReasons_hero.jpg"
+        alt="logo"
+      />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+        }}
+      >
+        {data.map((props) => newCard(props))}
+      </div>
     </div>
   );
 }
-
-export default App;
